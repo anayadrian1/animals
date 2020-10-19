60831d27920584a55db6bb4b9268be9f052b8efd
Adrian Anaya <aanaya79@cnm.edu>
2020-10-19 10:09:10 -0600

    Changed Asynctask to reactivex java.

--- app/src/main/java/edu/cnm/deepdive/animals/viewmodel/AnimalViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/animals/viewmodel/AnimalViewModel.java
@@ -14,6 +14,7 @@ import edu.cnm.deepdive.animals.BuildConfig;
 import edu.cnm.deepdive.animals.model.Animal;
 import edu.cnm.deepdive.animals.model.ApiKey;
 import edu.cnm.deepdive.animals.service.AnimalService;
+import io.reactivex.schedulers.Schedulers;
 import java.io.IOException;
 import java.util.List;
 import retrofit2.Response;
@@ -22,55 +23,35 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AnimalViewModel  extends AndroidViewModel {
 
-  private MutableLiveData<List<Animal>> animals;
+  private final MutableLiveData<List<Animal>> animals;
+  private final MutableLiveData<Throwable> throwable;
+  private final AnimalService animalService;
 
   public AnimalViewModel(@NonNull Application application) {
     super(application);
     animals = new MutableLiveData<>();
+    throwable = new MutableLiveData<>();
+    animalService = AnimalService.getInstance();
     loadAnimals();
   }
 
   public LiveData<List<Animal>> getAnimals() {
     return animals;
   }
-  @SuppressLint("StaticFieldLeak")
+
+  public MutableLiveData<Throwable> getThrowable() {
+    return throwable;
+  }
+
+  @SuppressLint("CheckResult")
   private void loadAnimals() {
-    new AsyncTask<Void, Void, List<Animal>>() {
 
-      AnimalService animalService;
-
-      @Override
-      protected void onPreExecute() {
-        super.onPreExecute();
-        Gson gson = new GsonBuilder()
-            .excludeFieldsWithoutExposeAnnotation()
-            .create();
-        Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(BuildConfig.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build();
-        animalService = retrofit.create(AnimalService.class);
-      }
-
-      @Override
-      protected List<Animal> doInBackground(Void... voids) {
-
-        try {
-          Response<ApiKey> keyResponse = animalService.getApiKey().execute();
-          ApiKey apiKey = keyResponse.body();
-          final String key = apiKey.getKey();
-
-          Response<List<Animal>> listResponse = animalService.getAnimals(key).execute();
-          List<Animal> animals = listResponse.body();
-          AnimalViewModel.this.animals.postValue(animals);
-          return animals;
-        } catch(IOException e) {
-          Log.e("AnimalService", e.getMessage(), e);
-          cancel(true);
-        }
-
-        return null;
-      }
-    }.execute();
+    animalService.getApiKey()
+        .subscribeOn(Schedulers.io())
+        .flatMap((key) -> animalService.getAnimals(key.getKey()))
+        .subscribe(
+            animals::postValue,
+            throwable::postValue
+        );
   }
 }
